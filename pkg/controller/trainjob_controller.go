b3adeec580dc70b431d9a1649803e81a985e0559
Antonin Stefanutti <antonin@stefanutti.fr>
2025-05-02 11:48:50 +0200

    Append ellipsis to event message if it's truncated
    Signed-off-by: Antonin Stefanutti <antonin@stefanutti.fr>

--- pkg/controller/trainjob_controller.go
+++ pkg/controller/trainjob_controller.go
@@ -128,8 +128,11 @@ func (r *TrainJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 			//  the creation of the runtime resources failed and the TrainJob is backed off until
 			//  the next retry attempt.
 			// The event message is truncated to stay within the maximum length limit (1024 chars).
-			r.recorder.Eventf(&trainJob, corev1.EventTypeWarning, "TrainJobResourcesCreationFailed",
-				"TrainJob resources reconciliation failed: %.950v", err.Error())
+			message := fmt.Sprintf("TrainJob resources reconciliation failed: %.950v", err.Error())
+			if len(err.Error()) > 950 {
+				message = fmt.Sprintf("%s ...", message)
+			}
+			r.recorder.Event(&trainJob, corev1.EventTypeWarning, "TrainJobResourcesCreationFailed", message)
 		}
 	}
 
