7f712afb286884c906a91d4ec6da65ebfa4d2e09
Xinmin Du <2812493086@qq.com>
2025-06-13 20:21:35 +0800

    using sort legacy
    Signed-off-by: Xinmin Du <2812493086@qq.com>

--- hack/e2e-setup-cluster.sh
+++ hack/e2e-setup-cluster.sh
@@ -48,13 +48,13 @@ print_cluster_info() {
   kubectl describe pod -n ${NAMESPACE}
 }
 
-# This avoids a race condition where API server has not yet registered CRDs
-echo "Deploy Kubeflow Trainer CRDs first"
-kubectl apply --server-side -k manifests/base/crds || (
-  kubectl logs -n ${NAMESPACE} -l app.kubernetes.io/name=trainer &&
-    print_cluster_info &&
-    exit 1
-)
+## This avoids a race condition where API server has not yet registered CRDs
+#echo "Deploy Kubeflow Trainer CRDs first"
+#kubectl apply --server-side -k manifests/base/crds || (
+#  kubectl logs -n ${NAMESPACE} -l app.kubernetes.io/name=trainer &&
+#    print_cluster_info &&
+#    exit 1
+#)
 
 echo "Deploy Kubeflow Trainer"
 E2E_MANIFESTS_DIR="artifacts/e2e/manifests"
