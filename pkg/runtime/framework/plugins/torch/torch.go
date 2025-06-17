0079881054ce2fb242be81335f6ddda39a40b067
Electronic-Waste <2690692950@qq.com>
2025-06-17 11:19:39 +0000

    fix(lint): fix lint error.
    Signed-off-by: Electronic-Waste <2690692950@qq.com>

--- pkg/runtime/framework/plugins/torch/torch.go
+++ pkg/runtime/framework/plugins/torch/torch.go
@@ -238,7 +238,7 @@ func calculateNumProcPerNode(
 		if shouldUseCPU(resources) {
 			cpuQ := resources[corev1.ResourceCPU]
 			return intstr.FromInt32(max(defaultCPU, int32(cpuQ.Value()))), true
-		} else if fallbackNumProcPerNode.Type != intstr.String{
+		} else if fallbackNumProcPerNode.Type != intstr.String {
 			gpuQ, ok := resources[corev1.ResourceName(constants.DeviceNvidiaGPU)]
 			if ok && gpuQ.Value() > 0 {
 				return intstr.FromInt32(int32(gpuQ.Value())), false
