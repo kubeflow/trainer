79618cb1e1e9db3f5febb7419f47dc73bb7f4f4a
Xinmin Du <2812493086@qq.com>
2025-08-10 18:32:23 +0800

    fix: add volcano to scheme
    Signed-off-by: Xinmin Du <2812493086@qq.com>

--- pkg/util/testing/client.go
+++ pkg/util/testing/client.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 
+	trainer "github.com/kubeflow/trainer/v2/pkg/apis/trainer/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -27,8 +28,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	jobsetv1alpha2 "sigs.k8s.io/jobset/api/jobset/v1alpha2"
 	schedulerpluginsv1alpha1 "sigs.k8s.io/scheduler-plugins/apis/scheduling/v1alpha1"
-
-	trainer "github.com/kubeflow/trainer/v2/pkg/apis/trainer/v1alpha1"
+	volcanov1beta1 "volcano.sh/apis/pkg/apis/scheduling/v1beta1"
 )
 
 func NewClientBuilder(addToSchemes ...func(s *runtime.Scheme) error) *fake.ClientBuilder {
@@ -37,6 +37,7 @@ func NewClientBuilder(addToSchemes ...func(s *runtime.Scheme) error) *fake.Clien
 	utilruntime.Must(trainer.AddToScheme(scm))
 	utilruntime.Must(jobsetv1alpha2.AddToScheme(scm))
 	utilruntime.Must(schedulerpluginsv1alpha1.AddToScheme(scm))
+	utilruntime.Must(volcanov1beta1.AddToScheme(scm))
 	for i := range addToSchemes {
 		utilruntime.Must(addToSchemes[i](scm))
 	}
