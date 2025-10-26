8bb88a51b9c4dcb322d485e9db9cadb1fc147389
vsoch <vsoch@users.noreply.github.com>
2025-10-26 13:45:27 -0700

    feat: support for flux framework as hpc manager
    Flux supports the majority of MPI flavors/variants, and can be used
    to bootstrap MPI as a plugin. It adds other features for scheduling
    and topology that can be used for simulations and ai/ml jobs.
    This changeset adds the plugin implementation, including the
    plugin module, tests, and an example with a small README to
    serve as documentation for the time being.
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- api/python_api/kubeflow_trainer_api/models/trainer_v1alpha1_flux_ml_policy_source.py
+++ api/python_api/kubeflow_trainer_api/models/trainer_v1alpha1_flux_ml_policy_source.py
@@ -17,22 +17,17 @@ import pprint
 import re  # noqa: F401
 import json
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, StrictInt
 from typing import Any, ClassVar, Dict, List, Optional
-from kubeflow_trainer_api.models.trainer_v1alpha1_flux_ml_policy_source import TrainerV1alpha1FluxMLPolicySource
-from kubeflow_trainer_api.models.trainer_v1alpha1_mpiml_policy_source import TrainerV1alpha1MPIMLPolicySource
-from kubeflow_trainer_api.models.trainer_v1alpha1_torch_ml_policy_source import TrainerV1alpha1TorchMLPolicySource
 from typing import Optional, Set
 from typing_extensions import Self
 
-class TrainerV1alpha1MLPolicySource(BaseModel):
+class TrainerV1alpha1FluxMLPolicySource(BaseModel):
     """
-    MLPolicySource represents the runtime-specific configuration for various technologies. One of the following specs can be set.
+    FluxMLPolicySource represents a Flux HPC runtime configuration.
     """ # noqa: E501
-    flux: Optional[TrainerV1alpha1FluxMLPolicySource] = Field(default=None, description="flux defines the configuration for the Flux runtime.")
-    mpi: Optional[TrainerV1alpha1MPIMLPolicySource] = Field(default=None, description="mpi defines the configuration for the MPI Runtime.")
-    torch: Optional[TrainerV1alpha1TorchMLPolicySource] = Field(default=None, description="torch defines the configuration for the PyTorch runtime.")
-    __properties: ClassVar[List[str]] = ["flux", "mpi", "torch"]
+    num_proc_per_node: Optional[StrictInt] = Field(default=None, description="numProcPerNode is the number of processes per node.", alias="numProcPerNode")
+    __properties: ClassVar[List[str]] = ["numProcPerNode"]
 
     model_config = ConfigDict(
         populate_by_name=True,
@@ -52,7 +47,7 @@ class TrainerV1alpha1MLPolicySource(BaseModel):
 
     @classmethod
     def from_json(cls, json_str: str) -> Optional[Self]:
-        """Create an instance of TrainerV1alpha1MLPolicySource from a JSON string"""
+        """Create an instance of TrainerV1alpha1FluxMLPolicySource from a JSON string"""
         return cls.from_dict(json.loads(json_str))
 
     def to_dict(self) -> Dict[str, Any]:
@@ -73,20 +68,11 @@ class TrainerV1alpha1MLPolicySource(BaseModel):
             exclude=excluded_fields,
             exclude_none=True,
         )
-        # override the default output from pydantic by calling `to_dict()` of flux
-        if self.flux:
-            _dict['flux'] = self.flux.to_dict()
-        # override the default output from pydantic by calling `to_dict()` of mpi
-        if self.mpi:
-            _dict['mpi'] = self.mpi.to_dict()
-        # override the default output from pydantic by calling `to_dict()` of torch
-        if self.torch:
-            _dict['torch'] = self.torch.to_dict()
         return _dict
 
     @classmethod
     def from_dict(cls, obj: Optional[Dict[str, Any]]) -> Optional[Self]:
-        """Create an instance of TrainerV1alpha1MLPolicySource from a dict"""
+        """Create an instance of TrainerV1alpha1FluxMLPolicySource from a dict"""
         if obj is None:
             return None
 
@@ -94,9 +80,7 @@ class TrainerV1alpha1MLPolicySource(BaseModel):
             return cls.model_validate(obj)
 
         _obj = cls.model_validate({
-            "flux": TrainerV1alpha1FluxMLPolicySource.from_dict(obj["flux"]) if obj.get("flux") is not None else None,
-            "mpi": TrainerV1alpha1MPIMLPolicySource.from_dict(obj["mpi"]) if obj.get("mpi") is not None else None,
-            "torch": TrainerV1alpha1TorchMLPolicySource.from_dict(obj["torch"]) if obj.get("torch") is not None else None
+            "numProcPerNode": obj.get("numProcPerNode")
         })
         return _obj
 
