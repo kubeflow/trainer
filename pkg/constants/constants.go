120e4ce9b323931391b5343fbf672d1c845f70e7
vsoch <vsoch@users.noreply.github.com>
2026-03-13 04:11:45 -0700

    feat: customize R generation to include GPUs
    Flux detection of GPUs depends on hwloc plugins, and a newer
    version. To get around any edge cases where the dependencies are missing,
    we can easily generate the R from the expected resource spec (cores
    and gpus). We can also add a shared memory mount to ensure the job
    MPI gets all available shared memory of the host. The current
    default is 64M (automatic from container runtime) and it can have
    implications for MPI performance.
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -119,6 +119,9 @@ const (
 	// Path for Flux curve path
 	FluxCurveVolumePath = "/curve"
 
+	// Ensure MPI has full memory of the host
+	FluxMemoryVolumeName = "shared-memory"
+
 	// emptyDir volume using for complete spack view software
 	FluxSpackViewVolumeName = "spack-install"
 
