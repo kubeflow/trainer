8d92d22a8032d4dd5c7bf22d84fcf1b74b23e498
krishdef7 <gargkrish06@gmail.com>
2026-03-17 21:20:39 +0530

    feat(framework): add generic EnforceRuntimeInfoPlugin interface
    Introduce EnforceRuntimeInfoPlugin, a generic plugin interface for plugins
    that update runtime.Info without being tied to a specific policy API.
    
    Migrate two existing plugins to this interface:
    - JobSet plugin: was using PodNetworkPlugin (IdentifyPodNetwork), which had
      the same signature but an overly specific name tied to network concerns.
    - TrainJobStatus plugin: was incorrectly using EnforceMLPolicyPlugin despite
      having no relation to ML policy.
    
    Remove PodNetworkPlugin interface as it is fully replaced by
    EnforceRuntimeInfoPlugin.
    
    Follow-up to #3227.
    Closes #3347
    
    Signed-off-by: krishdef7 <gargkrish06@gmail.com>

--- pkg/runtime/framework/interface.go
+++ pkg/runtime/framework/interface.go
@@ -51,9 +51,15 @@ type EnforceMLPolicyPlugin interface {
 	EnforceMLPolicy(info *runtime.Info, trainJob *trainer.TrainJob) error
 }
 
-type PodNetworkPlugin interface {
+// EnforceRuntimeInfoPlugin is a generic plugin interface for plugins that need
+// to update the runtime.Info object without being tied to a specific policy API
+// (ML policy, pod group policy, etc.).
+// This replaces the ad-hoc PodNetworkPlugin interface and the incorrect use of
+// EnforceMLPolicyPlugin for the TrainJobStatus plugin. Both are always activated
+// and operate purely on the runtime.Info object.
+type EnforceRuntimeInfoPlugin interface {
 	Plugin
-	IdentifyPodNetwork(info *runtime.Info, trainJob *trainer.TrainJob) error
+	EnforceRuntimeInfo(info *runtime.Info, trainJob *trainer.TrainJob) error
 }
 
 type ComponentBuilderPlugin interface {
