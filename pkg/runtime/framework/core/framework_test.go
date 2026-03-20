4f8727f7ea6765b3d8682513b8c27ad2e7fb246a
Harsh <harxhist@gmail.com>
2026-03-20 19:38:37 +0530

    Update framework_test.go
    Signed-off-by: Harsh <harxhist@gmail.com>

--- pkg/runtime/framework/core/framework_test.go
+++ pkg/runtime/framework/core/framework_test.go
@@ -2198,10 +2198,7 @@ test-job-node-0-1.test-job slots=1
 		cmpopts.SortSlices(func(a, b apiruntime.Object) bool {
 			return a.GetObjectKind().GroupVersionKind().String() < b.GetObjectKind().GroupVersionKind().String()
 		}),
-		// Compare MPI Secret data contents, but ignore DefaultMode differences on Secret volumes,
-		// which are validated more directly in MPI-specific tests.
 		cmp.Comparer(testingutil.MPISecretDataComparer),
-		cmpopts.IgnoreFields(corev1.SecretVolumeSource{}, "DefaultMode"),
 	}
 	for name, tc := range cases {
 		t.Run(name, func(t *testing.T) {
