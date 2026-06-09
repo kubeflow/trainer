3be3d1773e33dcc4be6ddff3a8a8972838c9bf61
Abhijeet Dhumal <84722973+abhijeet-dhumal@users.noreply.github.com>
2026-06-10 01:51:50 +0530

    fix: apply clientConnection QPS/burst to the manager client (#3432)
    * fix: Apply clientConnection QPS/burst to the manager's REST client
    
    Signed-off-by: abhijeet-dhumal <abhijeetdhumal652@gmail.com>
    
    * chore: Regenerate Helm README for clientConnection values
    
    Signed-off-by: abhijeet-dhumal <abhijeetdhumal652@gmail.com>
    
    * refactor: convert test cases from slices to maps per CONTRIBUTING.md convention
    
    Signed-off-by: abhijeet-dhumal <abhijeetdhumal652@gmail.com>
    
    * fix: drop redundant default in Helm template and simplify smoke test
    
    Signed-off-by: abhijeet-dhumal <abhijeetdhumal652@gmail.com>
    
    * fix: address review feedback - update ApplyClientConnection doc, drop redundant test, fractional QPS in values
    
    Signed-off-by: abhijeet-dhumal <abhijeetdhumal652@gmail.com>
    
    * fix: adjust generated readme
    
    Signed-off-by: abhijeet-dhumal <abhijeetdhumal652@gmail.com>
    
    * chore: run gofmt on config_test.go
    
    Signed-off-by: abhijeet-dhumal <abhijeetdhumal652@gmail.com>
    
    * fix failing Helm e2e test PR check
    
    Signed-off-by: abhijeet-dhumal <abhijeetdhumal652@gmail.com>
    
    ---------
    
    Signed-off-by: abhijeet-dhumal <abhijeetdhumal652@gmail.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -23,6 +23,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -131,6 +132,19 @@ func Load(scheme *runtime.Scheme, configFile string, enableHTTP2 bool) (ctrl.Opt
 	addTo(&options, &cfg, enableHTTP2)
 
 	return options, cfg, nil
+}
+
+// ApplyClientConnection copies QPS and burst from cfg.ClientConnection to restCfg.
+// If ClientConnection is nil or individual fields are nil, existing restCfg values are preserved.
+func ApplyClientConnection(restCfg *rest.Config, cfg *configapi.Configuration) {
+	if cfg.ClientConnection != nil {
+		if cfg.ClientConnection.QPS != nil {
+			restCfg.QPS = *cfg.ClientConnection.QPS
+		}
+		if cfg.ClientConnection.Burst != nil {
+			restCfg.Burst = int(*cfg.ClientConnection.Burst)
+		}
+	}
 }
 
 // IsCertManagementEnabled returns true if certificate management is enabled.
