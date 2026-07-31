8e20358d146a9a0328994797fe012a3fadd50d94
aniket2405 <aniketshaha2001@gmail.com>
2026-07-31 12:25:36 +0530

    feat(api): introduce OptimizationJob CRD under Trainer

---
+++ pkg/apis/trainer/v1alpha1/optimizationjob_types.go
@@ -0,0 +1,238 @@
+/*
+Copyright The Kubeflow Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+// +kubebuilder:validation:Enum=Maximize;Minimize
+type ObjectiveDirection string
+
+const (
+	ObjectiveDirectionMaximize ObjectiveDirection = "Maximize"
+	ObjectiveDirectionMinimize ObjectiveDirection = "Minimize"
+)
+
+type Objective struct {
+	// Metric specifies the name of the objective metric to track. Defaults to "loss".
+	// +kubebuilder:default=loss
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=64
+	// +optional
+	Metric *string `json:"metric,omitempty"`
+
+	// Direction specifies the optimization goal. Defaults to "Minimize".
+	// +kubebuilder:default=Minimize
+	// +optional
+	Direction *ObjectiveDirection `json:"direction,omitempty"`
+}
+
+// OptimizationJobSpec defines the desired state of OptimizationJob.
+// +kubebuilder:validation:XValidation:rule="self.parallelTrials <= self.numTrials",message="parallelTrials cannot exceed numTrials"
+type OptimizationJobSpec struct {
+	// +listType=map
+	// +listMapKey=metric
+	// +kubebuilder:validation:MinItems=1
+	// +kubebuilder:validation:MaxItems=1
+	// +required
+	Objectives []Objective `json:"objectives"`
+
+	// +optional
+	SearchAlgorithm *SearchAlgorithm `json:"searchAlgorithm,omitempty"`
+
+	// +listType=map
+	// +listMapKey=name
+	// +kubebuilder:validation:MinItems=1
+	// +kubebuilder:validation:MaxItems=100
+	// +required
+	Parameters []Parameter `json:"parameters"`
+
+	// NumTrials is the total number of trials to run.
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=100
+	// +optional
+	NumTrials *int32 `json:"numTrials,omitempty"`
+
+	// ParallelTrials is the number of trials to run in parallel. Defaults to 1.
+	// +kubebuilder:default=1
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=100
+	// +optional
+	ParallelTrials *int32 `json:"parallelTrials,omitempty"`
+
+	// +required
+	TrainJobTemplate TrainJobTemplateSpec `json:"trainJobTemplate"`
+}
+
+// +kubebuilder:validation:XValidation:rule="[has(self.random), has(self.grid)].filter(x, x).size() == 1",message="Exactly one search algorithm configuration must be provided"
+type SearchAlgorithm struct {
+	// +optional
+	Random *RandomAlgorithm `json:"random,omitempty"`
+	// +optional
+	Grid *GridAlgorithm `json:"grid,omitempty"`
+}
+
+type RandomAlgorithm struct {
+	// +optional
+	Seed *int64 `json:"seed,omitempty"`
+}
+
+type GridAlgorithm struct{}
+
+// +kubebuilder:validation:Enum=Int;Float
+type ParameterType string
+
+const (
+	ParameterTypeInt   ParameterType = "Int"
+	ParameterTypeFloat ParameterType = "Float"
+)
+
+// SearchSpace acts as a Discriminated Union (OneOf) supporting flexible statistical distributions.
+// +kubebuilder:validation:XValidation:rule="[has(self.uniform), has(self.logUniform), has(self.categorical)].filter(x, x).size() == 1",message="Exactly one search space distribution configuration must be provided"
+type SearchSpace struct {
+	// +optional
+	Uniform *UniformSpace `json:"uniform,omitempty"`
+
+	// +optional
+	LogUniform *LogUniformSpace `json:"logUniform,omitempty"`
+
+	// +optional
+	Categorical *CategoricalSpace `json:"categorical,omitempty"`
+}
+
+// +kubebuilder:validation:Pattern="^-?(0|[1-9][0-9]*)(\\.[0-9]+)?([eE][+-]?[0-9]+)?$"
+// +kubebuilder:validation:MaxLength=64
+type Double string
+
+// UniformSpace defines a continuous uniform distribution over [Min, Max].
+// +kubebuilder:validation:XValidation:rule="double(self.min) < double(self.max)",message="min must be strictly less than max"
+type UniformSpace struct {
+	// +required
+	Min Double `json:"min"`
+
+	// +required
+	Max Double `json:"max"`
+
+	// Type specifies the underlying data type. Defaults to "Float".
+	// +kubebuilder:default=Float
+	// +required
+	Type ParameterType `json:"type"`
+}
+
+// LogUniformSpace defines a continuous log-uniform distribution over [Min, Max].
+// +kubebuilder:validation:XValidation:rule="double(self.min) > 0.0",message="min must be strictly greater than 0"
+// +kubebuilder:validation:XValidation:rule="double(self.min) < double(self.max)",message="min must be strictly less than max"
+type LogUniformSpace struct {
+	// +required
+	Min Double `json:"min"`
+
+	// +required
+	Max Double `json:"max"`
+
+	// Type specifies the underlying data type. Defaults to "Float".
+	// +kubebuilder:default=Float
+	// +required
+	Type ParameterType `json:"type"`
+}
+
+// CategoricalSpace defines a search space over a discrete set of unordered strings.
+type CategoricalSpace struct {
+	// Choices is the set of strings to sample from.
+	// +kubebuilder:validation:MinItems=1
+	// +kubebuilder:validation:MaxItems=100
+	// +listType=set
+	// +required
+	Choices []string `json:"choices"`
+}
+
+type Parameter struct {
+	// Name is the name of the hyperparameter.
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=64
+	// +required
+	Name string `json:"name"`
+
+	// +required
+	SearchSpace SearchSpace `json:"searchSpace"`
+}
+
+// ParameterAssignment represents a single hyperparameter and its assigned value.
+type ParameterAssignment struct {
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=64
+	// +required
+	Name string `json:"name"`
+
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=64
+	// +required
+	Value string `json:"value"`
+}
+
+type TrainJobTemplateSpec struct {
+	// +optional
+	// +kubebuilder:validation:XValidation:rule="!has(self.name) && !has(self.namespace)", message="name and namespace cannot be set in a template."
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// +required
+	Spec TrainJobSpec `json:"spec,omitzero"`
+}
+
+type OptimizationJobStatus struct {
+	// +listType=map
+	// +listMapKey=type
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// +optional
+	Result *Result `json:"result,omitempty"`
+}
+
+// Result tracks the parameters of the highest performing trial.
+type Result struct {
+	// TrainJobName is the name of the underlying TrainJob that achieved this result.
+	// +kubebuilder:validation:MinLength=1
+	// +required
+	TrainJobName string `json:"trainJobName"`
+
+	// +listType=map
+	// +listMapKey=name
+	// +kubebuilder:validation:MaxItems=100
+	// +optional
+	Parameters []ParameterAssignment `json:"parameters,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// OptimizationJob is the Schema for the optimizationjobs API.
+type OptimizationJob struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   OptimizationJobSpec   `json:"spec,omitempty"`
+	Status OptimizationJobStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:object:root=true
+// OptimizationJobList contains a list of OptimizationJob.
+type OptimizationJobList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []OptimizationJob `json:"items"`
+}
---
+++ pkg/webhooks/optimizationjob_webhook_test.go
@@ -0,0 +1,150 @@
+/*
+Copyright The Kubeflow Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package webhooks
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"k8s.io/klog/v2/ktesting"
+	"k8s.io/utils/ptr"
+
+	trainer "github.com/kubeflow/trainer/v2/pkg/apis/trainer/v1alpha1"
+)
+
+func TestOptimizationJobDefault(t *testing.T) {
+	cases := map[string]struct {
+		inputObj *trainer.OptimizationJob
+		wantObj  *trainer.OptimizationJob
+	}{
+		"Empty limits and algorithms get defaulted": {
+			inputObj: &trainer.OptimizationJob{
+				Spec: trainer.OptimizationJobSpec{},
+			},
+			wantObj: &trainer.OptimizationJob{
+				Spec: trainer.OptimizationJobSpec{
+					ParallelTrials: ptr.To(int32(1)), // Defaulted
+					NumTrials:      ptr.To(int32(1)), // Defaulted
+					SearchAlgorithm: &trainer.SearchAlgorithm{
+						Random: &trainer.RandomAlgorithm{}, // Defaulted
+					},
+				},
+			},
+		},
+		"Existing limits and algorithms are preserved": {
+			inputObj: &trainer.OptimizationJob{
+				Spec: trainer.OptimizationJobSpec{
+					SearchAlgorithm: &trainer.SearchAlgorithm{
+						Grid: &trainer.GridAlgorithm{},
+					},
+					ParallelTrials: ptr.To(int32(5)),
+					NumTrials:      ptr.To(int32(20)),
+				},
+			},
+			wantObj: &trainer.OptimizationJob{
+				Spec: trainer.OptimizationJobSpec{
+					SearchAlgorithm: &trainer.SearchAlgorithm{
+						Grid: &trainer.GridAlgorithm{}, // Preserved
+					},
+					ParallelTrials: ptr.To(int32(5)),  // Preserved
+					NumTrials:      ptr.To(int32(20)), // Preserved
+				},
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			_, ctx := ktesting.NewTestContext(t)
+			defaulter := &OptimizationJobDefaulter{}
+
+			if err := defaulter.Default(ctx, tc.inputObj); err != nil {
+				t.Fatalf("Default returned unexpected error: %v", err)
+			}
+
+			if diff := cmp.Diff(tc.wantObj, tc.inputObj); len(diff) != 0 {
+				t.Errorf("Unexpected defaulting result (-want, +got): %s", diff)
+			}
+		})
+	}
+}
+
+func TestOptimizationJobValidateUpdate(t *testing.T) {
+	oldObj := &trainer.OptimizationJob{
+		Spec: trainer.OptimizationJobSpec{
+			Objectives: []trainer.Objective{
+				{Metric: ptr.To("loss")},
+			},
+			Parameters: []trainer.Parameter{
+				{Name: "learning_rate"},
+			},
+			TrainJobTemplate: trainer.TrainJobTemplateSpec{},
+			NumTrials:        ptr.To(int32(10)),
+		},
+	}
+
+	cases := map[string]struct {
+		updateFn func(obj *trainer.OptimizationJob)
+		wantErr  bool
+	}{
+		"Valid update: modifying metadata labels": {
+			updateFn: func(obj *trainer.OptimizationJob) {
+				if obj.Labels == nil {
+					obj.Labels = make(map[string]string)
+				}
+				obj.Labels["new-label"] = "foo"
+			},
+			wantErr: false,
+		},
+		"Invalid update: changing NumTrials": {
+			updateFn: func(obj *trainer.OptimizationJob) {
+				obj.Spec.NumTrials = ptr.To(int32(20))
+			},
+			wantErr: true,
+		},
+		"Invalid update: changing ParallelTrials": {
+			updateFn: func(obj *trainer.OptimizationJob) {
+				obj.Spec.ParallelTrials = ptr.To(int32(5))
+			},
+			wantErr: true,
+		},
+		"Invalid update: changing parameters": {
+			updateFn: func(obj *trainer.OptimizationJob) {
+				obj.Spec.Parameters = []trainer.Parameter{
+					{Name: "batch_size"},
+				}
+			},
+			wantErr: true,
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			_, ctx := ktesting.NewTestContext(t)
+			validator := &OptimizationJobValidator{}
+
+			// Create a deep copy to mutate
+			newObj := oldObj.DeepCopy()
+			tc.updateFn(newObj)
+
+			_, err := validator.ValidateUpdate(ctx, oldObj, newObj)
+			if (err != nil) != tc.wantErr {
+				t.Errorf("ValidateUpdate() error = %v, wantErr %v", err, tc.wantErr)
+			}
+		})
+	}
+}
