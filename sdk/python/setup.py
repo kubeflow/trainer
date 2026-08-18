74eac10089fdf620502132aee6ba2e4701c90c38
Andrey Velichkevich <andrey.velichkevich@gmail.com>
2026-08-18 18:48:13 +0100

    Release Training SDK 1.9.4
    Signed-off-by: Andrey Velichkevich <andrey.velichkevich@gmail.com>

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -37,7 +37,7 @@ REQUIRES = [
 
 setuptools.setup(
     name="kubeflow-training",
-    version="1.9.3",
+    version="1.9.4",
     author="Kubeflow Authors",
     author_email="hejinchi@cn.ibm.com",
     license="Apache License Version 2.0",
